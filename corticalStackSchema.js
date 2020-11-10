4333d0b16be32e790fb465b2b88df588cf2a9746
R-Aniruddha <aniruddha04@gmail.com>
2020-11-10 18:16:27 +0100

    Adding Schemas

---
+++ corticalStackSchema.js
@@ -0,0 +1,31 @@
+import CorticalStack from './corticalStack'
+import { EntitySchema } from 'typeorm'
+
+export const envelopeSchema = new EntitySchema({
+    tableName: 'table_stack',
+    name: 'corticalStackData',
+    target: CorticalStack,
+    columns:{
+        id: {
+            primary: true,
+            generated: true,
+            type: 'int'
+        },
+        realGender: {
+            type: 'varchar',
+            nullable: false
+        },
+        name: {
+            type: 'varchar',
+            nullable: false
+        },
+        age: {
+            type: 'int',
+            nullable: false
+        },
+        idStack: {
+            type: 'int',
+            nullable: false
+        }
+    }
+})
---
+++ envelopeSchema.js
@@ -0,0 +1,27 @@
+import Envelope from './Envelope'
+import { EntitySchema } from 'typeorm'
+
+export const envelopeSchema = new EntitySchema({
+    tableName: 'table_envelope',
+    name: 'envelopeData',
+    target: Envelope,
+    columns:{
+        id: {
+            primary: true,
+            generated: true,
+            type: 'int'
+        },
+        gender: {
+            type: 'varchar',
+            nullable: false
+        },
+        age: {
+            type: 'int',
+            nullable: false
+        },
+        idStack: {
+            type: 'int',
+            nullable: false
+        }
+    }
+})
